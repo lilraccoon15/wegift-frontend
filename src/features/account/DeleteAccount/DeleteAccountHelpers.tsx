a05ed06fee787062c38596eb22abbc7db1d77f85
clefort <camille.lefort@emundus.fr>
2025-06-09 11:03:12 +0200

    feature: delete account

---
+++ src/features/account/DeleteAccount/DeleteAccountHelpers.tsx
@@ -0,0 +1,25 @@
+import API_URL from "../../../config";
+import type { Account } from "../MyAccountHelpers";
+
+interface DeleteAccountPayload {
+    password: string;
+}
+
+export async function deleteAccount(data: DeleteAccountPayload): Promise<Account> {
+    const response = await fetch(`${API_URL}/api/auth/delete-account`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(data)
+    });
+
+    if(!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Erreur lors de la suppression du compte.");
+    }
+
+    const deleteAccount: Account = await response.json();
+    return deleteAccount;
+}
--- src/pages/Account/Preferences.tsx
+++ src/pages/Account/Preferences.tsx
@@ -2,46 +2,98 @@ import { useEffect, useState } from "react";
 import { useMyAccount } from "../../features/account/MyAccountHelpers";
 import { updateNewsletter } from "../../features/account/Preferences/PreferencesHelpers";
 import EditNewsletter from "../../features/account/Preferences/PreferenceNewsletter";
+import DeleteAccount from "../../features/account/DeleteAccount/DeleteAccount";
+import { deleteAccount } from "../../features/account/DeleteAccount/DeleteAccountHelpers";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Preferences = () => {
-    const { data: account, error, isLoading: loading } = useMyAccount();
+  const { data: account, error, isLoading: loading } = useMyAccount();
 
-    const [newsletter, setNewsletter] = useState<boolean>(
-        account?.newsletter ?? false
-    );
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [submitError, setSubmitError] = useState<string | null>(null);
+  const [newsletter, setNewsletter] = useState<boolean>(
+    account?.newsletter ?? false
+  );
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmittingDelete, setIsSubmittingDelete] = useState(false);
+  const [submitError, setSubmitError] = useState<string | null>(null);
+  const [submitErrorDelete, setSubmitErrorDelete] = useState<string | null>(
+    null
+  );
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [password, setPassword] = useState("");
+  const { logout } = useAuth();
+  const navigate = useNavigate();
 
-    const handleCheckboxChange = async (
-        e: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        const newValue = e.target.checked;
-        setNewsletter(newValue);
-        setIsSubmitting(true);
-        setSubmitError(null);
+  const buttonClassName = "";
 
-        try {
-            await updateNewsletter({ newsletter: newValue });
-            alert("Préférence de newsletter modifiée avec succès !");
-        } catch (err: any) {
-            setSubmitError(err.message || "Erreur lors de la modification");
-            setNewsletter(!newValue);
-        } finally {
-            setIsSubmitting(false);
-        }
-    };
+  const handleCheckboxChange = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newValue = e.target.checked;
+    setNewsletter(newValue);
+    setIsSubmitting(true);
+    setSubmitError(null);
 
-    if (loading) return <p>Chargement...</p>;
-    if (error) return <p>Erreur : {error.message}</p>;
+    try {
+      await updateNewsletter({ newsletter: newValue });
+      alert("Préférence de newsletter modifiée avec succès !");
+    } catch (err: any) {
+      setSubmitError(err.message || "Erreur lors de la modification");
+      setNewsletter(!newValue);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
-    return (
-        <EditNewsletter
-            newsletter={newsletter}
-            handleCheckboxChange={handleCheckboxChange}
-            isSubmitting={isSubmitting}
-            submitError={submitError}
-        />
-    );
+  const handleShowConfirmation = () => setShowConfirmation(true);
+  const handleHideConfirmation = () => setShowConfirmation(false);
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleDeleteAccount = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmittingDelete(true);
+    setSubmitErrorDelete(null);
+
+    try {
+      await deleteAccount({ password });
+      await logout();
+      navigate("/");
+    } catch (err: any) {
+      setSubmitErrorDelete(
+        err.message || "Erreur lors de la suppression du compte"
+      );
+    } finally {
+      setIsSubmittingDelete(false);
+    }
+  };
+
+  if (loading) return <p>Chargement...</p>;
+  if (error) return <p>Erreur : {error.message}</p>;
+
+  return (
+    <div>
+      <EditNewsletter
+        newsletter={newsletter}
+        handleCheckboxChange={handleCheckboxChange}
+        isSubmitting={isSubmitting}
+        submitError={submitError}
+      />
+
+      <DeleteAccount
+        showConfirmation={showConfirmation}
+        setShowConfirmation={handleShowConfirmation}
+        setHideConfirmation={handleHideConfirmation}
+        password={password}
+        setPassword={handlePasswordChange}
+        submitError={submitErrorDelete}
+        buttondisabled={isSubmittingDelete}
+        handleDeleteAccount={handleDeleteAccount}
+      />
+    </div>
+  );
 };
 
 export default Preferences;
