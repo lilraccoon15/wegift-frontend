3558010e84d9f6c8f849a0a9a605311c67ae8ba1
clefort <camille.lefort@emundus.fr>
2025-07-28 15:31:01 +0200

    mep

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,39 +3,40 @@ import { useAuth } from "../context/AuthContext";
 import Dashboard from "./Dashboard";
 
 const Home = () => {
-    const { isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-    if (isAuthenticated) {
-        return <Dashboard />;
-    }
+  if (isAuthenticated) {
+    return <Dashboard />;
+  }
 
-    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL_AUTH;
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL_AUTH;
+  console.log("AUTH ENV →", import.meta.env.VITE_BACKEND_URL_AUTH);
 
-    const PICTURE = "/img/home.jpg";
+  const PICTURE = "/img/home.jpg";
 
-    return (
-        <div className="frame">
-            <img
-                src={`${BACKEND_URL}${PICTURE}`}
-                alt="personnes s'offrant des cadeaux"
-                className="home-picture"
-            />
-            <h2>Offrir n’a jamais été aussi simple !</h2>
-            <span>
-                Créez des moments inoubliables avec <strong>WeGift</strong>.
-            </span>
+  return (
+    <div className="frame">
+      <img
+        src={`${BACKEND_URL}${PICTURE}`}
+        alt="personnes s'offrant des cadeaux"
+        className="home-picture"
+      />
+      <h2>Offrir n’a jamais été aussi simple !</h2>
+      <span>
+        Créez des moments inoubliables avec <strong>WeGift</strong>.
+      </span>
 
-            <Link to="/register" className="btn">
-                Créer un compte
-            </Link>
-            <p>
-                Déjà inscrit ?{" "}
-                <Link to="/login" className="link">
-                    Se connecter
-                </Link>
-            </p>
-        </div>
-    );
+      <Link to="/register" className="btn">
+        Créer un compte
+      </Link>
+      <p>
+        Déjà inscrit ?{" "}
+        <Link to="/login" className="link">
+          Se connecter
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
