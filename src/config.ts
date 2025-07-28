80637d09b4df6b3113e01d7c5594bf4a49887dfc
clefort <camille.lefort@emundus.fr>
2025-07-28 15:43:50 +0200

    mep suite

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default API_URL;
--- src/features/auth/CreatePassword/CreatePasswordHelpers.ts
+++ src/features/auth/CreatePassword/CreatePasswordHelpers.ts
@@ -1,25 +1,26 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const createPassword = async (
-    password: string
+  password: string
 ): Promise<{ ok: boolean }> => {
-    try {
-        const response = await fetch(`${API_URL}/api/auth/create-password`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify({ password }),
-        });
+  try {
+    const response = await fetch(`${API_URL}/api/auth/create-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ password }),
+    });
 
-        if (!response.ok) {
-            return { ok: false };
-        }
-
-        return { ok: true };
-    } catch (error) {
-        console.error("Erreur lors de la création du mot de passe :", error);
-        return { ok: false };
+    if (!response.ok) {
+      return { ok: false };
     }
+
+    return { ok: true };
+  } catch (error) {
+    console.error("Erreur lors de la création du mot de passe :", error);
+    return { ok: false };
+  }
 };
