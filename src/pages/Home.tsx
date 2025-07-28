cd2ead5ea70a78979f13c1bb684701c45f16762c
lilraccoon15 <camille.lefort15@gmail.com>
2025-07-28 20:31:02 +0200

    test vite

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,39 +3,43 @@ import { useAuth } from "../context/AuthContext";
 import Dashboard from "./Dashboard";
 
 const Home = () => {
-  const { isAuthenticated } = useAuth();
+    const { isAuthenticated } = useAuth();
 
-  if (isAuthenticated) {
-    return <Dashboard />;
-  }
+    if (isAuthenticated) {
+        return <Dashboard />;
+    }
 
-  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL_AUTH;
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL_AUTH;
+    console.log(
+        "🏗️ VITE_BACKEND_URL_AUTH =",
+        import.meta.env.VITE_BACKEND_URL_AUTH
+    );
 
-  const PICTURE = "/img/home.jpg";
+    const PICTURE = "/img/home.jpg";
 
-  return (
-    <div className="frame">
-      <img
-        src={`${BACKEND_URL}${PICTURE}`}
-        alt="personnes s'offrant des cadeaux"
-        className="home-picture"
-      />
-      <h2>Offrir n’a jamais été aussi simple !</h2>
-      <span>
-        Créez des moments inoubliables avec <strong>WeGift</strong>.
-      </span>
+    return (
+        <div className="frame">
+            <img
+                src={`${BACKEND_URL}${PICTURE}`}
+                alt="personnes s'offrant des cadeaux"
+                className="home-picture"
+            />
+            <h2>Offrir n’a jamais été aussi simple !</h2>
+            <span>
+                Créez des moments inoubliables avec <strong>WeGift</strong>.
+            </span>
 
-      <Link to="/register" className="btn">
-        Créer un compte
-      </Link>
-      <p>
-        Déjà inscrit ?{" "}
-        <Link to="/login" className="link">
-          Se connecter
-        </Link>
-      </p>
-    </div>
-  );
+            <Link to="/register" className="btn">
+                Créer un compte
+            </Link>
+            <p>
+                Déjà inscrit ?{" "}
+                <Link to="/login" className="link">
+                    Se connecter
+                </Link>
+            </p>
+        </div>
+    );
 };
 
 export default Home;
