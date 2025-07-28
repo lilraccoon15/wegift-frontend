c2f81dddc01c2f7638f1315689d4d899f892fee0
lilraccoon15 <camille.lefort15@gmail.com>
2025-07-28 20:35:41 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,22 @@ export default defineConfig(({ mode }) => {
             port: 3000,
         },
         define: {
+            // Pour process.env (optionnel si tu veux le garder)
             "process.env": env,
+            // 🔥 Pour forcer Vite à injecter les variables côté client :
+            "import.meta.env.VITE_BACKEND_URL_AUTH": JSON.stringify(
+                env.VITE_BACKEND_URL_AUTH
+            ),
+            "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+            "import.meta.env.VITE_BACKEND_URL_WISHLIST": JSON.stringify(
+                env.VITE_BACKEND_URL_WISHLIST
+            ),
+            "import.meta.env.VITE_BACKEND_URL_USER": JSON.stringify(
+                env.VITE_BACKEND_URL_USER
+            ),
+            "import.meta.env.VITE_BACKEND_URL_EXCHANGE": JSON.stringify(
+                env.VITE_BACKEND_URL_EXCHANGE
+            ),
         },
     };
 });
