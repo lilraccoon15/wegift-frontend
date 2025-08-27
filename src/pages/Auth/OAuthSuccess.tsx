64f0a1ed41a24e0dde9f7fff3f0b975437ed0bff
clefort <camille.lefort@emundus.fr>
2025-08-27 12:09:45 +0200

    fix google callback

--- src/pages/Auth/OAuthSuccess.tsx
+++ src/pages/Auth/OAuthSuccess.tsx
@@ -3,25 +3,21 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 const OAuthSuccess = () => {
-    const navigate = useNavigate();
-    const { loginWithToken } = useAuth();
+  const navigate = useNavigate();
+  const { loginWithToken } = useAuth();
 
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const token = params.get("token");
+  useEffect(() => {
+    // Plus besoin de token en query
+    loginWithToken()
+      .then(() => {
+        navigate("/");
+      })
+      .catch(() => {
+        navigate("/login?error=oauth");
+      });
+  }, []);
 
-        if (token) {
-            document.cookie = `token=${token}; path=/; SameSite=Lax`;
-
-            loginWithToken();
-
-            navigate("/");
-        } else {
-            navigate("/login?error=oauth");
-        }
-    }, []);
-
-    return <p>Connexion via Google en cours...</p>;
+  return <p>Connexion via Google en cours...</p>;
 };
 
 export default OAuthSuccess;
