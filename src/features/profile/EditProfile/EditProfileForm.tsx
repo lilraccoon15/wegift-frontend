a5738608006c6f97bf6a164d21f913897d87ac2c
lilraccoon15 <camille.lefort15@gmail.com>
2025-08-31 15:13:01 +0200

    testing new notification bell

--- src/features/profile/EditProfile/EditProfileForm.tsx
+++ src/features/profile/EditProfile/EditProfileForm.tsx
@@ -5,113 +5,115 @@ import Message from "../../../components/ui/Message";
 import { DEFAULT_PICTURES, BACKEND_URLS } from "../../../config/constants";
 
 interface EditProfilFormProps {
-  pseudo: string;
-  birthDate: Date;
-  picturePreview?: string | null;
-  description: string;
-  onPseudoChange: (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => void;
-  onBirthDateChange: (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => void;
-  onPictureChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onDescriptionChange: (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => void;
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  error: string | null;
-  buttondisabled: boolean;
+    pseudo: string;
+    birthDate: Date;
+    picturePreview?: string | null;
+    description: string;
+    onPseudoChange: (
+        e: React.ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+        >
+    ) => void;
+    onBirthDateChange: (
+        e: React.ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+        >
+    ) => void;
+    onPictureChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onDescriptionChange: (
+        e: React.ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+        >
+    ) => void;
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    error: string | null;
+    buttondisabled: boolean;
 }
 
+// todo : check la vérif (majuscule pseudo en l'occurence)
+
 const EditProfileForm: React.FC<EditProfilFormProps> = ({
-  pseudo,
-  birthDate,
-  picturePreview,
-  description,
-  onPseudoChange,
-  onBirthDateChange,
-  onDescriptionChange,
-  onPictureChange,
-  onSubmit,
-  error,
-  buttondisabled,
+    pseudo,
+    birthDate,
+    picturePreview,
+    description,
+    onPseudoChange,
+    onBirthDateChange,
+    onDescriptionChange,
+    onPictureChange,
+    onSubmit,
+    error,
+    buttondisabled,
 }) => {
-  const birthDateString = birthDate.toISOString().substring(0, 10);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
+    const birthDateString = birthDate.toISOString().substring(0, 10);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const handlePictureClick = () => {
-    fileInputRef.current?.click();
-  };
+    const handlePictureClick = () => {
+        fileInputRef.current?.click();
+    };
 
-  return (
-    <form
-      onSubmit={onSubmit}
-      encType="multipart/form-data"
-      className="no-modal-form"
-    >
-      <div className="edit-profile-picture">
-        <div
-          className="profile-picture"
-          style={{
-            backgroundImage: `url('${
-              picturePreview
-                ? picturePreview.startsWith("blob:")
-                  ? picturePreview
-                  : picturePreview.startsWith("http")
-                  ? picturePreview
-                  : `${BACKEND_URLS.user}${picturePreview}`
-                : `${BACKEND_URLS.user}${DEFAULT_PICTURES.user}`
-            }')`,
-          }}
-          onClick={handlePictureClick}
-        />
-        <input
-          type="file"
-          accept="image/*"
-          ref={fileInputRef}
-          style={{ display: "none" }}
-          onChange={onPictureChange}
-        />
-        <small>Cliquez sur la photo pour la modifier</small>
-      </div>
+    return (
+        <form
+            onSubmit={onSubmit}
+            encType="multipart/form-data"
+            className="no-modal-form"
+        >
+            <div className="edit-profile-picture">
+                <div
+                    className="profile-picture"
+                    style={{
+                        backgroundImage: `url('${
+                            picturePreview
+                                ? picturePreview.startsWith("blob:")
+                                    ? picturePreview
+                                    : picturePreview.startsWith("http")
+                                    ? picturePreview
+                                    : `${BACKEND_URLS.user}${picturePreview}`
+                                : `${BACKEND_URLS.user}${DEFAULT_PICTURES.user}`
+                        }')`,
+                    }}
+                    onClick={handlePictureClick}
+                />
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    style={{ display: "none" }}
+                    onChange={onPictureChange}
+                />
+                <small>Cliquez sur la photo pour la modifier</small>
+            </div>
 
-      <label>Pseudo :</label>
-      <InputField
-        type="text"
-        placeholder="Pseudo"
-        value={pseudo}
-        onChange={onPseudoChange}
-        required
-      />
-      <label>Date de naissance :</label>
-      <InputField
-        type="date"
-        name="birthDate"
-        value={birthDateString}
-        onChange={onBirthDateChange}
-        required
-      />
-      <label>Description :</label>
-      <InputField
-        isTextArea
-        placeholder="Description"
-        value={description}
-        onChange={onDescriptionChange}
-        className="textarea-style"
-      />
-      {error && <Message text={error} type="error" />}
-      <Button type="submit" disabled={buttondisabled}>
-        Modifier
-      </Button>
-    </form>
-  );
+            <label>Pseudo :</label>
+            <InputField
+                type="text"
+                placeholder="Pseudo"
+                value={pseudo}
+                onChange={onPseudoChange}
+                required
+            />
+            <label>Date de naissance :</label>
+            <InputField
+                type="date"
+                name="birthDate"
+                value={birthDateString}
+                onChange={onBirthDateChange}
+                required
+            />
+            <label>Description :</label>
+            <InputField
+                isTextArea
+                placeholder="Description"
+                value={description}
+                onChange={onDescriptionChange}
+                className="textarea-style"
+            />
+            {error && <Message text={error} type="error" />}
+            <Button type="submit" disabled={buttondisabled}>
+                Modifier
+            </Button>
+        </form>
+    );
 };
 
 export default EditProfileForm;
